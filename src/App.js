e50073fa1ffe38619742ec47eaaf890187e4145e
Freddy Drennan <drennanfreddy@gmail.com>
2020-02-03 09:51:55 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,63 +1,92 @@
-import React, { useState } from 'react';
-import DataTable from './DataTable';
-import Inputs from "./Inputs";
+import React, { useState } from "react";
+import DataTable from "./DataTable";
 
-const PORT = 6287;
+// Global starting variables
+const PORT = 5906;
 
 function App() {
-
-    const baseUrlCreator = (PORT, ticker, startDate, endDate) => {
-        return `http://127.0.0.1:${PORT}/get_stocks_data?stocks=["${ticker}"]&startDate=${startDate}&endDate=${endDate}`;
-    };
-
-    const [userQuery, setUserQuery] = useState('AAPL');
-    const [baseUrl, setBaseUrl] = useState(baseUrlCreator(PORT, 'AAPL', '2019-01-01', '2020-01-01'));
-    const [priorSearches, updatePriorSearches] = useState([baseUrl]);
-
-    const updateUserQuery = event => {
-        event.preventDefault();
-      setUserQuery(event.target.value);
-    };
-
-    const searchQuery = () => {
-        const newBaseUrl = baseUrlCreator(PORT, userQuery, '2019-01-01', '2020-01-01');
-        setBaseUrl(newBaseUrl);
-        updatePriorSearches([
-            ...priorSearches, newBaseUrl
-        ])
-        setUserQuery('');
-    };
-
-    const handleKeyPress = event => {
-      if (event.key === 'Enter') {
-        searchQuery();
-      }
+  // Starting Function
+  const baseUrlCreator = (port, ticker, startDate, endDate) => {
+    console.log("\n\n\n\nInside baseUrlCreator");
+    return `http://127.0.0.1:${port}/get_stocks_data?stocks=["${ticker}"]&startDate=${startDate}&endDate=${endDate}`;
   };
 
+  // APPLICATION STARTING VARIABLES
+  const [userQuery, setUserQuery] = useState("AAPL");
+  const [userStartDate, setUserStartDate] = useState("2019-01-01");
+  const [userEndDate, setUserEndDate] = useState("2019-03-01");
+  const initialBaseUrl = baseUrlCreator(
+    PORT,
+    userQuery,
+    userStartDate,
+    userEndDate
+  );
+  const [baseUrl, setBaseUrl] = useState(initialBaseUrl);
+  const [priorSearches, setPriorSearches] = useState([]);
 
+  const searchQuery = () => {
+    console.log("Inside searchQuery");
+    const newBaseUrl = baseUrlCreator(
+      PORT,
+      userQuery,
+      userStartDate,
+      userEndDate
+    );
+    // #1
+    setBaseUrl(newBaseUrl);
+    // #2
+    setPriorSearches([...priorSearches, newBaseUrl]);
+  };
+
+  const updateUserQuery = event => {
+    console.log("Inside updateUserQuery");
+    event.preventDefault();
+    setUserQuery(event.target.value);
+  };
+
+  const handleKeyPress = event => {
+    console.log("Inside handleKeyPress");
+    if (event.key === "Enter") {
+      searchQuery();
+    }
+  };
+
+  const handleStartDate = event => {
+    console.log("Inside handleStartDate");
+    setUserStartDate(event.target.value);
+  };
+
+  const handleEndDate = event => {
+    console.log("Inside handleStartDate");
+    setUserEndDate(event.target.value);
+  };
 
   return (
     <div className="App">
-      <h1>Hello David</h1>
-      <div className='form' onSubmit={searchQuery}>
-        <input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress} />
+      <h1>Stock Explorer</h1>
+      <div className="form" onSubmit={searchQuery}>
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyPress={handleKeyPress}
+        />
+        <input type="date" value={userStartDate} onChange={handleStartDate} />
+        <input type="date" value={userEndDate} onChange={handleEndDate} />
         <button onClick={searchQuery}>Search</button>
       </div>
       <hr />
-      <Inputs />
-      <DataTable baseUrl={baseUrl} setBaseUrl={setBaseUrl}/>
+      <DataTable baseUrl={baseUrl} setBaseUrl={setBaseUrl} />
       <h1>Old Searches</h1>
-        <hr/>
-        {
-            priorSearches.map((x) => {
-                return (
-                    <div>
-                        <p>{x}</p>
-                    </div>
-                )
-            })
-        }
-        <hr/>
+      <hr />
+      {priorSearches.map((list, index) => {
+        console.log(list);
+        return (
+          <div key={index}>
+            <p>{list}</p>
+          </div>
+        );
+      })}
+      <hr />
     </div>
   );
 }
--- src/DataTable.js
+++ src/DataTable.js
@@ -1,71 +1,75 @@
-import React from 'react';
-import { useFetch } from './hooks';
-import {CSVLink} from "react-csv";
-
-
-// const PORT = 6287;
-
-// let baseUrl = `http://127.0.0.1:${PORT}/get_stocks_data?stocks=["${'AAPL'}"]&startDate=${'2019-01-01'}&endDate=${'2019-02-01'}`;
+import React, { useEffect, useState } from "react";
+import { useFetch } from "./hooks";
+import { CSVLink } from "react-csv";
 
 function DataTable(props) {
-
   const { message, data } = useFetch(props.baseUrl, {});
 
-  const cleanData = data => {
+  const json2array = json => {
+    console.log("Inside json2array");
+    let result = [];
+    let keys = Object.keys(json);
+    keys.forEach(function(key) {
+      result.push(json[key]);
+    });
+
+    return result;
+  };
+
+  const jsonToCSV = data => {
+    console.log("Inside jsonToCSV");
     if (data) {
-        const dataMap =  (
-            JSON.parse(data).map(
-                (x) => {
-                    return (
-                                <tr key={x.date}>
-                                    <td key={1}>{x.date}</td>
-                                    <td key={2}>{x.open}</td>
-                                    <td key={3}>{x.high}</td>
-                                    <td key={4}>{x.low}</td>
-                                    <td key={5}>{x.close}</td>
-                                    <td key={6}>{x.volume}</td>
-                                    <td key={7}>{x.adjusted}</td>
-                                </tr>
-
-                    )
-                }
-            )
-        );
-
-        return (
-
-                <table>
-                    <tbody>
-                    <tr>
-                        <td>Date</td>
-                        <td>Open</td>
-                        <td>High</td>
-                        <td>Low</td>
-                        <td>Close</td>
-                        <td>Volume</td>
-                        <td>Adjusted</td>
-                    </tr>
-                     {dataMap}
-                    </tbody>
-                </table>
-
-        );
+      console.log("Inside jsonToCSV:data");
+      const returnedData = JSON.parse(data);
+      return json2array(returnedData);
     }
   };
 
-  const json2array = json => {
-        var result = [];
-        var keys = Object.keys(json);
-        keys.forEach(function(key){
-            result.push(json[key]);
-        });
-        return result;
-    };
+  const cleanData = data => {
+    console.log("Inside cleanData");
 
-  const jsonToCSV = data => {
-    if(data) {
-        const returnedData = JSON.parse(data);
-        return json2array(returnedData);
+    if (data) {
+      console.log("Inside cleanData:if(data)");
+      const parseData = JSON.parse(data);
+
+      const sortedData = parseData.sort(function(a, b) {
+        if (a.date < b.date) return 1;
+        if (b.date < a.date) return -1;
+        return 0;
+      });
+
+      const dataMap = sortedData.map((x, index) => {
+        return (
+          <tr key={index}>
+            <td key={0}>{x.symbol}</td>
+            <td key={1}>{x.date}</td>
+            <td key={2}>{x.open}</td>
+            <td key={3}>{x.high}</td>
+            <td key={4}>{x.low}</td>
+            <td key={5}>{x.close}</td>
+            <td key={6}>{x.volume}</td>
+            <td key={7}>{x.adjusted}</td>
+          </tr>
+        );
+      });
+
+      return (
+        <table>
+          <tbody>
+            <tr key={"tbody"}>
+              <td key={0}>Symbol</td>
+              <td key={1}>Date</td>
+              <td key={2}>Open</td>
+              <td key={3}>High</td>
+              <td key={4}>Low</td>
+              <td key={5}>Close</td>
+              <td key={6}>Volume</td>
+              <td key={7}>Adjusted</td>
+            </tr>
+            {dataMap}
+          </tbody>
+        </table>
+      );
     }
   };
 
@@ -73,14 +77,22 @@ function DataTable(props) {
 
   return (
     <div>
-      <h3>MetaData</h3>
-        {data ? <CSVLink data={jsonToCSV(data)}
-                         filename={"stock-data.csv"}
-                         target="data">Download Me</CSVLink> : <p>Missing</p>}
-        <p><em>{message}</em></p>
-        {parsedData}
+      {data ? (
+        <CSVLink
+          data={jsonToCSV(data)}
+          filename={"stock-data.csv"}
+          target="data"
+        >
+          Download Me
+        </CSVLink>
+      ) : (
+        <p>
+          <em>{message}</em>
+        </p>
+      )}
+      {parsedData}
     </div>
-  )
+  );
 }
 
 export default DataTable;
