a0ccebb2072bfba07697a4e799e1f8d2ab242f58
Freddy Drennan <drennanfreddy@gmail.com>
2020-02-05 22:54:22 -0700

    Updates

---
+++ precache-manifest.763e4d14ae0bf75615e123e294d3f615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db936bccdb2a5207778d",
+    "url": "/stocks/static/css/main.c3d72299.chunk.css"
+  },
+  {
+    "revision": "db936bccdb2a5207778d",
+    "url": "/stocks/static/js/main.db936bcc.chunk.js"
+  },
+  {
+    "revision": "bf38fc93852ff617158a",
+    "url": "/stocks/static/js/runtime~main.bf38fc93.js"
+  },
+  {
+    "revision": "66ab1d035982eb3dc784",
+    "url": "/stocks/static/js/2.66ab1d03.chunk.js"
+  },
+  {
+    "revision": "6211f461b03b050e01e1f68d1f337592",
+    "url": "/stocks/index.html"
+  }
+];
